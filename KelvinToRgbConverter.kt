780b1b71c4073b30e446bc085760ca67d2e3c9bf
egek92 <oTQnodsT9dEp>
2021-01-26 13:59:04 +0100

    [REFACTOR] converter

--- KelvinToRgbConverter.kt
+++ KelvinToRgbConverter.kt
@@ -1,40 +1,48 @@
+import kotlin.math.*
+
+const val MAX = 255.0
+const val MIN = 0.0
+
+fun Float.toRgb() = convertKelvinToRgb(this)
+
 fun convertKelvinToRgb(kelvin: Float): Triple<Float, Float, Float> {
 
-        val temp = (kelvin / 100.0)
+    val temp = (kelvin.div(100.0))
 
-        var red: Double?
-        var green: Double?
-        var blue: Double?
-
-        if (temp <= 66.0) {
-            red = 255.0
-            green = temp
-            green = 99.4708025861 * ln(green) - 161.1195681661
+    var red: Double?
+    var green: Double?
+    var blue: Double?
 
 
-            if (temp <= 19.0) {
-                blue = 0.0
-            } else {
-                blue = temp - 10.0
-                blue = 138.5177312231 * ln(blue) - 305.0447927307
-            }
+    if (temp <= 66.0) {
+        red = MAX
+        green = temp
+        green = (99.4708025861.times(ln(green))).minus(161.1195681661)
+
+
+        if (temp <= 19.0) {
+            blue = MIN
         } else {
-            red = temp - 60.0
-            red = 329.698727446 *  red.pow(-0.1332047592)
-            green = temp - 60.0
-            green = 288.1221695283 * green.pow(-0.0755148492)
-            blue = 255.0
+            blue = temp.minus(10.0)
+            blue = (138.5177312231.times(ln(blue))).minus(305.0447927307)
         }
-
-
-        return Triple(clamp(red, 0.0, 255.0), clamp(green, 0.0, 255.0), clamp(blue, 0.0, 255.0))
-
+    } else {
+        red = temp.minus(60.0)
+        red = 329.698727446.times(red.pow(-0.1332047592))
+        green = temp.minus(60.0)
+        green = 288.1221695283.times(green.pow(-0.0755148492))
+        blue = MAX
     }
 
-    private fun clamp(value: Double, min: Double, max: Double): Float {
-        if (value < min) return min.toFloat()
 
-        if (value > max) return max.toFloat()
+    return Triple(clamp(red, MIN, MAX), clamp(green, MIN, MAX), clamp(blue, MIN, MAX))
 
-        return value.toFloat()
-    }
+}
+
+private fun clamp(value: Double, min: Double, max: Double): Float {
+    if (value < min) return min.toFloat()
+
+    if (value > max) return max.toFloat()
+
+    return value.toFloat()
+}
